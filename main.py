8d83d01f82db7ae39501c7764e646f090960a153
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-09-04 18:10:27 +0530

    updated

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ def compare_folders(folder1, folder2, extensions_to_compare=None):
                 print(f"File '{common_file}' is identical in both folders.")
             else:
                 print(f"File '{common_file}' differs in content. (Differing in '{folder1}' and '{folder2}')")
+                show_file_differences(file1, file2)
                 fix_file = input(f"Do you want to fix '{common_file}'? (y/n): ")
                 if fix_file.lower() == 'y':
                     source_path = os.path.join(folder2, common_file)
                     destination_path = os.path.join(folder1, common_file)
                     shutil.copy2(source_path, destination_path)
-                    print(f"File '{common_file}' is copied to the first folder.")
+                    print(f"File '{common_file}' is copied to the second code folder.")
                     continue
                 else:
                     show_file_differences(file1, file2)
@@ -33,41 +34,41 @@ def compare_folders(folder1, folder2, extensions_to_compare=None):
         path = os.path.join(folder2, new_file)
         if os.path.isfile(path):
             if not extensions_to_compare or (extensions_to_compare and os.path.splitext(new_file)[1] in extensions_to_compare):
-                print(f"File '{new_file}' is new in the second folder. (Located in '{folder2}')")
+                print(f"File '{new_file}' is not present in the second folder. (Located in '{folder2}')")
                 fix_file = input(f"Do you want to fix '{new_file}'? (y/n): ")
                 if fix_file.lower() == 'y':
                     source_path = os.path.join(folder2, new_file)
                     destination_path = os.path.join(folder1, new_file)
                     shutil.copy2(source_path, destination_path)
-                    print(f"File '{new_file}' is copied to the first folder.")
+                    print(f"File '{new_file}' is copied to the second code folder.")
         elif os.path.isdir(path):
-            print(f"Directory '{new_file}' is new in the second folder. (Located in '{folder2}')")
+            print(f"Directory '{new_file}' is not present in the second folder. (Located in '{folder2}')")
             fix_dir = input(f"Do you want to fix '{new_file}'? (y/n): ")
             if fix_dir.lower() == 'y':
                 source_path = os.path.join(folder2, new_file)
                 destination_path = os.path.join(folder1, new_file)
                 shutil.copytree(source_path, destination_path)
-                print(f"Directory '{new_file}' is copied to the first folder.")
+                print(f"Directory '{new_file}' is copied to the second code folder.")
 
     for old_file in comparison.left_only:
         path = os.path.join(folder1, old_file)
         if os.path.isfile(path):
             if not extensions_to_compare or (extensions_to_compare and os.path.splitext(old_file)[1] in extensions_to_compare):
-                print(f"File '{old_file}' is old in the second folder. (Located in '{folder1}')")
+                print(f"File '{old_file}' is present only in second code folder. (Located in '{folder1}')")
                 fix_file = input(f"Do you want to fix '{old_file}'? (y/n): ")
                 if fix_file.lower() == 'y':
                     source_path = os.path.join(folder1, old_file)
                     destination_path = os.path.join(folder2, old_file)
                     shutil.copy2(source_path, destination_path)
-                    print(f"File '{old_file}' is copied to the second folder.")
+                    print(f"File '{old_file}' is copied to the main code folder.")
         elif os.path.isdir(path):
-            print(f"Directory '{old_file}' is old in the second folder. (Located in '{folder1}')")
+            print(f"Directory '{old_file}' is present only in second code folder. (Located in '{folder1}')")
             fix_dir = input(f"Do you want to fix '{old_file}'? (y/n): ")
             if fix_dir.lower() == 'y':
                 source_path = os.path.join(folder1, old_file)
                 destination_path = os.path.join(folder2, old_file)
                 shutil.copytree(source_path, destination_path)
-                print(f"Directory '{old_file}' is copied to the second folder.")
+                print(f"Directory '{old_file}' is copied to the main code folder.")
 
     # Recursively compare subdirectories
     for sub_dir in comparison.common_dirs:
